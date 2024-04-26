593ef8139dbfff4a6871427d1a1cacda4af502bc
Giuliopime <60524738+Giuliopime@users.noreply.github.com>
2024-04-26 13:28:08 +0200

    fix: ktor version and default values for place

--- src/main/kotlin/app/e20/data/models/event/EventPlaceData.kt
+++ src/main/kotlin/app/e20/data/models/event/EventPlaceData.kt
@@ -9,7 +9,7 @@ import kotlinx.serialization.Serializable
  */
 @Serializable
 data class EventPlaceData(
-    val name: String?,
+    val name: String? = null,
     val address: String,
-    val url: String?
+    val url: String? = null
 )
